7f1c77e2b74ce96dd98e03159636deb12ed5dee4
youhonami <youhonami42@gmail.com>
2026-03-10 22:24:18 +0900

    不要テーブル、カラムの削除

--- backend/src/database/seeders/UsersTableSeeder.php
+++ backend/src/database/seeders/UsersTableSeeder.php
@@ -15,25 +15,19 @@ class UsersTableSeeder extends Seeder
         User::create([
             'name' => 'テストユーザー1',
             'email' => 'user1@example.com',
-            'email_verified_at' => now(),
             'password' => Hash::make('password'), // ログイン用パスワード
-            'remember_token' => Str::random(10),
         ]);
 
         User::create([
             'name' => 'テストユーザー2',
             'email' => 'user2@example.com',
-            'email_verified_at' => now(),
             'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
         ]);
 
         User::create([
             'name' => 'テストユーザー3',
             'email' => 'user3@example.com',
-            'email_verified_at' => now(),
             'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
         ]);
     }
 }
